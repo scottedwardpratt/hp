5b7b0de38196c23e57ce1aee1fc8e598a7a75b24
Scott Pratt <scott@pratt-linux.nscl.msu.edu>
2020-04-03 19:20:15 -0400

    no changes

--- alice/fromcascade/bigrunner_b3d.sh
+++ alice/fromcascade/bigrunner_b3d.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-make b3d
+make b3d_fromcascade
 nproc=24
-nruns=40
 for ((i=0;i<${nproc};i+=1))
 do
-	firsti=`expr ${i} \* ${nruns}`;
-	lasti=`expr ${firsti} + ${nruns} - 1`;
-	echo firsti=${firsti}, lasti=${lasti};
-	`./runner_b3d.sh ${firsti} ${lasti} > logfiles/b3d_${firsti}_${lasti}.txt &` ;
+	`./runner_b3d.sh ${i} > logfiles/b3d_${i}.txt &` ;
 done
