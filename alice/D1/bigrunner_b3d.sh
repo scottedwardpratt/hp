ea2e3db3adf3f29490534b48e560234cbc2b719a
Scott Pratt <scott@pratt-linux.nscl.msu.edu>
2020-04-15 09:42:17 -0400

    changed scripts for running b3d_fromhydro

--- alice/D1/bigrunner_b3d.sh
+++ alice/D1/bigrunner_b3d.sh
@@ -5,7 +5,7 @@ nruns=30
 for ((i=0;i<${nproc};i+=1))
 do
 	firsti=`expr ${i} \* ${nruns}`;
-	lasti=`expr ${firsti} + ${nruns} - 1`;
+	lasti=`expr ${firsti} + ${nruns}`;
 	echo firsti=${firsti}, lasti=${lasti};
 	`./runner_b3d.sh ${firsti} ${lasti} > logfiles/b3d_${firsti}_${lasti}.txt &` ;
 done
