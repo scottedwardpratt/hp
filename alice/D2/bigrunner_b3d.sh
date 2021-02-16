867615507eeb2169e80618bb6bcb3c7a2acd828e
scottedwardpratt <prattsc@msu.edu>
2021-02-16 14:31:32 -0500

    adding default_sum to git repo

--- alice/D2/bigrunner_b3d.sh
+++ alice/D2/bigrunner_b3d.sh
@@ -5,8 +5,8 @@ nproc=24
 nruns=30
 for ((i=0;i<${nproc};i+=1))
 do
-	firsti=`expr ${i} \* ${nruns}`;
-	lasti=`expr ${firsti} + ${nruns} - 1`;
+	firsti=`expr ${i} \* ${nruns} + 25`;
+	lasti=`expr ${i} \* ${nruns} + ${nruns} - 1`;
 	rm -f logfiles/b3d_${firsti}_${lasti}.txt;
 	echo starting runs with firsti=${firsti}, lasti=${lasti};
 	`./runner_b3d.sh ${firsti} ${lasti} > logfiles/b3d_${firsti}_${lasti}.txt &` ;
