4f73283eea597bbab26796c63ad78c351b74f50c
scottedwardpratt <prattsc@msu.edu>
2021-02-22 10:22:07 -0500

    results

--- alice/bin/summer.cc
+++ alice/bin/summer.cc
@@ -16,6 +16,7 @@ int main(int argc,char *argv[]){
 	string dirprefix="default";
 	char qs[200];
 	const int NTYPES=10;
+	int Nqcut=4;
 	int NJDIR;
 	FILE *fptr_read;
 	FILE *fptr_write;
@@ -30,9 +31,8 @@ int main(int argc,char *argv[]){
 	scanf("%d",&NJDIR);
 	
 	qualifier=qs;
-	string olddatafilename[4]={"bf_phi.dat","bf_eta.dat","bf_y.dat","bf_qinv.dat"};
-	string newdatafilename[4]={"bf_phi.dat","bf_eta.dat","bf_y.dat","bf_qinv.dat"};
-	int Nqcut=4;
+	string olddatafilename[5]={"bf_phi.dat","bf_eta.dat","bf_y.dat","bf_eta1.dat","bf_qinv.dat"};
+	string newdatafilename[5]={"bf_phi.dat","bf_eta.dat","bf_y.dat","bf_eta1.dat","bf_qinv.dat"};
 	string dirname[NTYPES]={"KK","Kp","piK","pip","pipi","pp","allcharges","allcharges_phi0","allcharges_phi45","allcharges_phi90"};
 	string command;
 	for(itype=0;itype<NTYPES;itype++){
@@ -76,9 +76,9 @@ int main(int argc,char *argv[]){
 	// Now average balance funtions
 	for(itype=0;itype<NTYPES;itype++){
 		if(itype<6)
-			Nqcut=4;
+			Nqcut=5;
 		else
-			Nqcut=3;
+			Nqcut=4;
 		for(ifn=0;ifn<Nqcut;ifn++){
 			dx=0.0;
 			if(ifn==0)
@@ -88,6 +88,8 @@ int main(int argc,char *argv[]){
 			if(ifn==2)
 				dx=0.1;
 			if(ifn==3)
+				dx=0.1;
+			if(ifn==4)
 				dx=10.0;
 			newdirname="default_sum/"+qualifier+"/"+acceptance+"/"+dirname[itype];
 			command="mkdir -p "+newdirname;
@@ -105,7 +107,7 @@ int main(int argc,char *argv[]){
 				fptr_read=fopen(oldfilename.c_str(),"r");
 				ix=0;
 				fscanf(fptr_read,"%lf %lf",&x,&y[0]);
-				if(ifn>2)
+				if(ifn>3)
 					fscanf(fptr_read,"%lf %lf %lf",&y[1],&y[2],&y[3]);
 				fgets(dummy,100,fptr_read);
 
@@ -113,7 +115,7 @@ int main(int argc,char *argv[]){
 					if(jdir==0){
 						xsum.push_back(x);
 						ysum[0].push_back(y[0]);
-						if(ifn>2){
+						if(ifn>3){
 							ysum[1].push_back(y[1]);
 							ysum[2].push_back(y[2]);
 							ysum[3].push_back(y[3]);
@@ -121,7 +123,7 @@ int main(int argc,char *argv[]){
 					}
 					else{
 						ysum[0][ix]+=y[0];
-						if(ifn>2){
+						if(ifn>3){
 							ysum[1][ix]+=y[1];
 							ysum[2][ix]+=y[2];
 							ysum[3][ix]+=y[3];
@@ -129,7 +131,7 @@ int main(int argc,char *argv[]){
 					}
 					ix+=1;
 					fscanf(fptr_read,"%lf %lf",&x,&y[0]);
-					if(ifn>2)
+					if(ifn>3)
 						fscanf(fptr_read,"%lf %lf %lf",&y[1],&y[2],&y[3]);
 					fgets(dummy,100,fptr_read);
 				}
@@ -142,13 +144,13 @@ int main(int argc,char *argv[]){
 				if(ifn==0)
 					y[0]=0.5*(ysum[0][ix]+ysum[0][nx-ix-1]);
 				y[0]=y[0]/double(NJDIR);
-				if(ifn>2){
+				if(ifn>3){
 					y[1]=ysum[1][ix]/double(NJDIR);
 					y[2]=ysum[2][ix]/double(NJDIR);
 					y[3]=ysum[3][ix]/double(NJDIR);
 				}
 				fprintf(fptr_write,"%5.2f %10.3e",xsum[ix],y[0]);
-				if(ifn>2)
+				if(ifn>3)
 					fprintf(fptr_write," %10.3e %10.3e %10.3e",y[1],y[2],y[3]);
 				fprintf(fptr_write,"\n");
 				bfnorm+=y[0]*dx;
