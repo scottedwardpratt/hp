2876db318b2cc69ff9358eb1336b4bc4b2b485dc
scottedwardpratt <prattsc@msu.edu>
2021-03-07 19:27:06 -0500

    trajectory stuff

---
+++ alice/software/hydrobalance/src/trajectory.cc
@@ -0,0 +1,22 @@
+#include "charge.h"
+#include "misc.h"
+#include "hyper.h"
+#include "hydro2uds.h"
+#include "randy.h"
+#include "constants.h"
+
+using namespace std;
+
+CTrajInfo::CTrajInfo(int balanceIDset){
+	balanceID=balanceIDset;
+	string filename="trajectory/traj"+to_string(balanceID)+".txt";
+	fptr=fopen(filename.c_str(),"w");
+}
+
+void CTrajInfo::add(double x1,double y1,double eta1,double tau1){
+	x.push_back(x1);
+	y.push_back(y1);
+	eta.push_back(eta1);
+	tau.push_back(tau1);
+}
+
